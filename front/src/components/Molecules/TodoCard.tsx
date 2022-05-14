932cd6cf4704f228a24772862ea2f8c6984eeaf3
hibik17 <hibiki@macbook>
2022-05-14 10:30:10 +0900

    chage card clolor

--- front/src/components/Molecules/TodoCard.tsx
+++ front/src/components/Molecules/TodoCard.tsx
@@ -3,7 +3,10 @@ import { ToggleButton } from "../Atoms/ToggleButton";
 
 export const TodoCard = () => {
   return (
-    <div className="min-w-sm max-w-sm rounded overflow-hidden shadow-lg m-3 hover:-translate-y-1 hover:scale-110 hover:bg-teal-100 duration-300">
+    <div
+      className="min-w-sm max-w-sm bg-gradient-to-r from-rose-100 to-teal-100
+    rounded overflow-hidden shadow-lg m-3 hover:-translate-y-1 hover:scale-110 hover:bg-teal-100 duration-300"
+    >
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">Todo title</div>
         <p className="text-gray-700 text-base">
