c294f448b1d0c3cee5ca206bd8ee7d51b99133e5
hibik17 <hibiki@macbook>
2022-05-14 17:04:50 +0900

    sidebar 修正

--- front/src/components/Organisms/Header.tsx
+++ front/src/components/Organisms/Header.tsx
@@ -51,18 +51,7 @@ export const Header = () => {
           </a>
         </div>
         <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 m-3"
-          >
-            SignIn
-          </a>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            LogIn
-          </a>
+          <button>refresh button</button>
         </div>
       </div>
     </nav>
--- front/src/components/Organisms/Sidebar.tsx
+++ front/src/components/Organisms/Sidebar.tsx
@@ -1,67 +1,37 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useCallback, useEffect } from "react";
+
+// icon import from react-icon
+import {
+  AiOutlineCarryOut,
+  AiOutlinePlusSquare,
+  AiFillFire,
+} from "react-icons/ai";
+import { SidebarLink } from "../hooks/SidebarLink";
 
 export const Sidebar = () => {
-  // 画面遷移を行う
-  const navigate = useNavigate();
+  const sidebarArray = [
+    // all task page
+    ["/todos", <AiOutlineCarryOut className="w-6 h-6" />, "ALL task"],
+    // new task page
+    ["/newtodo", <AiOutlinePlusSquare className="w-6 h-6" />, "New Task"],
+    // today's task page
+    ["/today_todo", <AiFillFire className="w-6 h-6" />, "Today Task"],
+    // completed task page
+    ["/completed_todo", <AiFillFire className="w-6 h-6" />, "completed task"],
+  ];
 
   return (
     <div className="flex flex-col justify-between mt-6">
       <aside>
         <ul>
-          <li>
-            <button
-              className="flex w-full items-center px-4 py-2 text-gray-700 hover:bg-gray-300"
-              onClick={() => navigate("/todos")}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-
-              <span className="mx-4 font-medium">Home</span>
-            </button>
-          </li>
-
-          <li>
-            <button
-              className="flex w-full items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-300"
-              onClick={() => navigate("/newtodo")}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-
-              <span className="mx-4 font-medium">New Task</span>
-            </button>
-          </li>
+          {sidebarArray.map((sideItem, index) => (
+            <SidebarLink
+              linkPath={sideItem[0]}
+              linkIcon={sideItem[1]}
+              linkText={sideItem[2]}
+              key={index}
+            />
+          ))}
         </ul>
       </aside>
     </div>
